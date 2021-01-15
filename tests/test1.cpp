87838d5e316b6c12c5b441975e359e8405393605
Grant-Giesbrecht <giesbrechtgrant@gmail.com>
2021-01-15 01:35:55 -0700

    Added test file and test directory

---
+++ tests/test1.cpp
@@ -0,0 +1,19 @@
+/*
+This is a minimal complexity program for CX to use as a compilation test. If it
+can compile and run it, it will print the 'succcess' message below.
+*/
+
+#include <iostream>
+#include <stdio.h>
+
+//CXPRINTCOM FALSE
+
+using namespace std;
+
+int main(){
+
+	cout << "\n *** CX Installed Correctly *** \n" << endl;
+
+	return 0;
+
+}
---
+++ tests/test_quiet.cpp
@@ -0,0 +1,20 @@
+/*
+This is a minimal complexity program for CX to use as a compilation test. If it
+can compile and run it, it will print the 'succcess' message below.
+*/
+
+#include <iostream>
+#include <stdio.h>
+
+//CXPRINTCOM TRUE
+//CXCOMPILE echo "Testing CX"
+
+using namespace std;
+
+int main(){
+
+	//This doesnt do anything
+
+	return 0;
+
+}
