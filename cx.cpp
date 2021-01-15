601fdbaf053c1daf46cc8180d4175cb450815287
Grant-Giesbrecht <giesbrechtgrant@gmail.com>
2021-01-15 01:36:14 -0700

    cx.cpp: Automatically select compiler based on OS

--- cx.cpp
+++ cx.cpp
@@ -1,9 +1,16 @@
 //
 //  cx.cpp
-//  
+//
 //
 //  Created by Grant Giesbrecht on 5/3/19.
 //
+
+#ifdef __APPLE__
+	#define DEF_COMPILER "clang++"
+#else
+	#define DEF_COMPILER "g++"
+#endif
+
 
 #include <stdio.h>
 #include <iostream>
@@ -16,20 +23,20 @@
 using namespace std;
 
 int main(int argc, char** argv){
-	
+
 	//--------------------- DISPLAY HELP PAGE IF NO INPUTS ---------------
 	//
-	
+
 	if (argc < 2){
-		
+
 		cout << "No input arguments" << endl;
 		cout << "Use -h to view help" << endl;
-		
+
 		return -1;
 	}
-	
+
 	string first_arg = argv[1];
-	
+
 	if (first_arg == "-h" || first_arg == "-H" || first_arg == "-HELP" || first_arg == "-help" || first_arg == "-Help"){
 		cout << "************** CX HELP *****************\n" << endl;
 		cout << "CX automatically compiles and runs C++" << endl;
@@ -73,19 +80,19 @@ int main(int argc, char** argv){
 		cout << "Example usages (to run in command line):" << endl;
 		cout << "\tcx testprogram    OR    cx testprogram.cpp" << endl;
 		cout << "Example usage (to appear in testprogram.cpp):" << endl;
-		cout << "\t//CXCOMPILE clang++ -o output testprogram.cpp" << endl;
+		cout << "\t//CXCOMPILE " << string(DEF_COMPILER) << " -o output testprogram.cpp" << endl;
 		cout << "\t// CXPRINTCOM TRUE" << endl;
 		cout << "\t//CXGENRUN FALSE" << endl;
-		
+
 		return 0;
 	}
-	
+
 	//------------------- DETERMINE FILE NAME -------------------------
 	//
-	
+
 	string filename = argv[1]; //Filename WTIHOUT extension
 	string filename_w_ext = argv[1]; // Filename WITH extension
-	
+
 	//Scan for extension in 'argv[1]'
 	bool extension_found = false;
 	for (size_t i = 0 ; i < filename.length() ;  i++){
@@ -94,36 +101,36 @@ int main(int argc, char** argv){
 			extension_found = true; //Mark that an extension was found
 		}
 	}
-	
+
 	//Add extension to 'filename_w_ext', if neccesary
 	if (!extension_found){
 		filename_w_ext = filename_w_ext + ".cpp";
 	}
-	
+
 	ifstream file;
 	file.open(filename_w_ext);
 	if (!file.is_open()){
 		cout << "ERROR: Failed to open file '" << argv[1] << "'" << endl;
 		return -1;
 	}
-	
+
 	//-------------------- GENERATE COMPILER COMMANDS ------------------
 	//
-	
+
 	vector<string> compiler_commands;
 	bool print_commands = false; //Print commands before running them
 	bool verify_commands = false; //Verify commands with user before running them
 	bool generate_run_command = true; //CX must generate its own command to run the executable
 	bool persist_on_error = false; //If true, CX will continue executing commands even if one returns and error
-	
+
 	//Scan through file, look for
 	string line;
 	size_t line_number = 1;
 	while (file.good()){
-		
+
 		//Read next line
 		getline(file, line);
-		
+
 		//Look for compile commands
 		if (line.substr(0, 12) == "// CXCOMPILE"){
 			compiler_commands.push_back(line.substr(13)); //Add compiler command
@@ -132,9 +139,9 @@ int main(int argc, char** argv){
 			compiler_commands.push_back(line.substr(12)); //Add compiler command
 			continue;
 		}
-		
+
 		//Look for output filename commands (ie. name of thing to run)
-		
+
 		//Look for 'print commands' commands
 		if (line.substr(0, 13) == "// CXPRINTCOM"){
 			if (line.length() < 18){
@@ -159,7 +166,7 @@ int main(int argc, char** argv){
 			}
 			continue;
 		}
-		
+
 		//Look for 'verify commands' commands
 		if (line.substr(0, 11) == "// CXVERCOM"){
 			if (line.length() < 16){
@@ -184,7 +191,7 @@ int main(int argc, char** argv){
 			}
 			continue;
 		}
-		
+
 		//Look for 'verify commands' commands
 		if (line.substr(0, 11) == "// CXGENRUN"){
 			if (line.length() < 16){
@@ -209,7 +216,7 @@ int main(int argc, char** argv){
 			}
 			continue;
 		}
-		
+
 		//Look for 'print commands' commands
 		if (line.substr(0, 12) == "// CXPERSIST"){
 			if (line.length() < 17){
@@ -234,29 +241,29 @@ int main(int argc, char** argv){
 			}
 			continue;
 		}
-		
+
 		//Stop function once main function is declared
 		if (line.substr(0, 8) == "int main" || line.substr(0, 4) == "main"){
 			break;
 		}
-		
+
 		//Increment line number
 		line_number++;
-		
+
 	}
-	
+
 	//If no command was specified, generate a command from the filename
 	if (compiler_commands.size() < 1){
-		compiler_commands.push_back("clang++ -o " + filename + " " + filename_w_ext);
+		compiler_commands.push_back(string(DEF_COMPILER) + " -o " + filename + " " + filename_w_ext);
 	}
-	
+
 	//Add command to run code
 	if (generate_run_command){
 		compiler_commands.push_back("./" + filename);
 	}
-	
+
 	//------------------ PRINT AND RUN COMMANDS --------------------------
-	
+
 	//Print commands if requested
 	if (print_commands){
 		cout << "Will run: " << endl;
@@ -264,13 +271,13 @@ int main(int argc, char** argv){
 			cout << "\t[" << i+1 << ".] '" << compiler_commands[i] << "'" << endl;
 		}
 	}
-	
+
 	//Verify commands before running if requested
 	if (verify_commands){
 		string user_input;
 		cout << "Proceed? Enter 'Y' to continue: ";
 		getline(cin, user_input);
-		
+
 		if (user_input != "Y"){
 			return -1;
 		}
@@ -288,6 +295,6 @@ int main(int argc, char** argv){
 			}
 		}
 	}
-	
+
 	return 0;
 }
